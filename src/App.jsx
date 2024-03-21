e20c28835caeb0ab40d7ba5d90c8efd7614fee62
Vibu Roshin E V <viburoshin57@gmail.com>
2024-03-21 18:20:40 +0530

    new file:   .gitignore 	new file:   index.html 	new file:   package-lock.json 	new file:   package.json 	new file:   public/bg-pattern-dark.png 	new file:   public/bg-pattern.png 	new file:   public/game-logo.png 	new file:   src/App.jsx 	new file:   src/assets/react.svg 	new file:   src/components/Header.jsx 	new file:   src/index.css 	new file:   src/index.jsx 	new file:   vite.config.js

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import Header from "./components/Header";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <main>
+        <div id="game-container">
+          <ol id="players">
+            <li>
+              <span className="player-name">Player 1</span>
+              <span className="player-symbol">X</span>
+            </li>
+            <li>
+              <span className="player-name">Player 2</span>
+              <span className="player-symbol">O</span>
+            </li>
+          </ol>
+        </div>
+      </main>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <header>
+      <img src="../../public/game-logo.png" alt="tic-tac-toe" />
+      <h1>Tic-Tac-Toe</h1>
+    </header>
+  );
+};
+
+export default Header;
