020fa15591441e7c57343aa8429a9bc25df17c48
Vibu Roshin E V <viburoshin57@gmail.com>
2024-03-22 14:32:55 +0530

    modified:   src/App.jsx 	modified:   src/components/GameBoard.jsx 	new file:   src/components/Log.jsx 	modified:   src/components/Player.jsx

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 
 const initialGameBoard = [
   [null, null, null],
@@ -6,18 +6,27 @@ const initialGameBoard = [
   [null, null, null],
 ];
 
-const GameBoard = () => {
-  const [gameBoard, setGameBoard] = useState(initialGameBoard);
+const GameBoard = ({ onSelectSquare, turns }) => {
+  //const [gameBoard, setGameBoard] = useState(initialGameBoard);
 
-  const handleSelect = (rowIndex, columnIndex) => {
-    setGameBoard((prevGameBoard) => {
-      const updatedBoard = [
-        ...prevGameBoard.map((innerArray) => [...innerArray]),
-      ];
-      updatedBoard[rowIndex][columnIndex] = "X";
-      return updatedBoard;
-    });
-  };
+  // const handleSelect = (rowIndex, columnIndex) => {
+  //   setGameBoard((prevGameBoard) => {
+  //     const updatedBoard = [
+  //       ...prevGameBoard.map((innerArray) => [...innerArray]),
+  //     ];
+  //     updatedBoard[rowIndex][columnIndex] = activePlayerSymbol;
+  //     return updatedBoard;
+  //   });
+  //   onSelectSquare();
+  // };
+  let gameBoard = initialGameBoard;
+
+  for (const turn of turns) {
+    const { square, player } = turn;
+    const { row, col } = square;
+
+    gameBoard[row][col] = player;
+  }
   return (
     <ol id="game-board">
       {gameBoard.map((row, rowIndex) => (
@@ -25,7 +34,7 @@ const GameBoard = () => {
           <ol>
             {row.map((playerSymbol, columnIndex) => (
               <li key={columnIndex}>
-                <button onClick={() => handleSelect(rowIndex, columnIndex)}>
+                <button onClick={() => onSelectSquare(rowIndex, columnIndex)}>
                   {playerSymbol}
                 </button>
               </li>
