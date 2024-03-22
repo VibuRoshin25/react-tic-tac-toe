3c0f3d23e2defa8029a60c60684f7ecc7576f7f7
Vibu Roshin E V <viburoshin57@gmail.com>
2024-03-22 11:31:34 +0530

    Update Player.jsx

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,13 +1,25 @@
 import React from "react";
-
+import { useState } from "react";
 const Player = ({ name, symbol }) => {
+  const [isEditing, setIsEditing] = useState(false);
+
+  let playerName = <span className="player-name">{name}</span>;
+  let buttonVal = "Edit";
+
+  if (isEditing) {
+    playerName = <input type="text" name="" id="" required value={name} />;
+    buttonVal = "Save";
+  }
+
   return (
     <li>
       <span className="player">
-        <span className="player-name">{name}</span>
+        {playerName}
         <span className="player-symbol">{symbol}</span>
       </span>
-      <button>Edit</button>
+      <button onClick={() => setIsEditing((editing) => !editing)}>
+        {buttonVal}
+      </button>
     </li>
   );
 };
